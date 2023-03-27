e3335809b3d4705ac74316adb1281d87bfb6d871
Vuk Djordjevic <vukd10@gmail.com>
2023-03-27 17:22:23 +0200

    Added base functionality. Login working for now.

--- src/client/game_client.py
+++ src/client/game_client.py
@@ -1,6 +1,32 @@
+import json
+import struct
+
+from server_enum import Action
 from service import Service
 
 
 class GameClient:
     def __init__(self) -> None:
-        pass
+        self.__service = Service()
+        self.__service.connect("wgforge-srv.wargaming.net", 443)
+
+    def __del__(self) -> None:
+        self.__service.disconnect()
+
+    def login(self, name: str) -> int:
+        d: dict = {
+            "name": name
+        }
+
+        msg: bytes = bytes(json.dumps(d), 'utf-8')
+        out: bytes = struct.pack('ii', Action.LOGIN, len(msg)) + msg
+
+        self.__service.send_data(out)
+        ret = self.__service.receive_data()
+        print(ret)
+        return 0
+
+
+if __name__ == "__main__":
+    c = GameClient()
+    c.login("MegatronJeremy")
--- src/client/service.py
+++ src/client/service.py
@@ -1,6 +1,26 @@
+import socket
+
+
 class Service:
     def __init__(self) -> None:
-        pass
+        self.__socket: socket = socket.socket()
+
+    def __del__(self) -> None:
+        self.disconnect()
+
+    def connect(self, host: str, port: int) -> None:
+        self.__socket.connect((host, port))
+
+    def send_data(self, out: bytes) -> bool:
+        ret: bool = self.__socket.send(out) > 0
+        return ret
+
+    def receive_data(self) -> str:
+        msg: str = self.__socket.recv(1024).decode()
+        return msg
+
+    def disconnect(self) -> None:
+        self.__socket.close()
 
     def function(self) -> None:
         pass
