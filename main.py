6e7b1b4016b2a49967c4b1ec40757dc81b3c103f
Vuk Djordjevic <vukd10@gmail.com>
2023-05-15 20:51:41 +0200

    Refactored main

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 from src.gui.display_manager import DisplayManager
-# from tests.multiplayer_game import multiplayer_game
-
 
 if __name__ == '__main__':
     display_manager = DisplayManager()
     display_manager.run()
-
-    # multiplayer_game()
-
-    # results_eval()
-
-    # train(num_trainings=5_000,
-    #       num_rounds=15,
-    #       restart=True,
-    #       num_players=3,
-    #       num_actions=1,
-    #       save_file='rnd1')
